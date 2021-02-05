b6d621785a39add82499b7a4a3d11397297de37c
grillow <spmr757@gmail.com>
2021-02-05 04:35:02 +0300

    fixed mkdir before cd

--- sort.sh
+++ sort.sh
@@ -3,17 +3,18 @@
 source ./mover.sh
 source ./config_reader.sh
 
-mkdir -p video
-mkdir -p website
-mkdir -p gif
-mkdir -p pic
-
 read_config video
 read_config website
 read_config gif
 read_config pic
 
 cd "$1"
+
+mkdir -p video
+mkdir -p website
+mkdir -p gif
+mkdir -p pic
+
 move_extensions video "${video_extensions[@]}"
 move_extensions website "${website_extensions[@]}"
 move_extensions gif "${gif_extensions[@]}"
